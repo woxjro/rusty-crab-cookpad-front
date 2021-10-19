bcc4f97898edfbba21ff3df215a77d760dc93bfa
woxjro <n0i6c0k2@gmail.com>
2021-10-19 20:52:23 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        rusy-crab-cookpad
+      </header>
+      <body>
+        <div></div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
